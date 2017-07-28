8720b4f73ea8ba25cf5235659cbaff3b03906865
alexandrefortin <fortin.alexandre@yahoo.fr>
2017-07-28 23:44:06 +0200

    Modify sample builder

--- angio_simu/simu/matlab/BuildMySample.m
+++ angio_simu/simu/matlab/BuildMySample.m
@@ -1,11 +1,12 @@
-Nx_gly=0; %Pr simu4 et simu5
-Nx_eau=0; %Pr simu1 et simu2 et simu3
-Nx_bld=112305+499*8609;
+%Set number of spins for each category
+Nx_blood=100;
+Nx_tissue1=0;
+Nx_tissue2=0;
 
 
 Nx0=Nx_gly;
 Nx1=Nx_gly+Nx_eau;
-Nx2=Nx_gly+Nx_eau+Nx_bld;
+Nx2=Nx_gly+Nx_eau+Nx_blood;
 
 
 DB=zeros(Nx1,1,1);
@@ -15,32 +16,29 @@ T2=zeros(Nx1,1,1);
 T2s=zeros(Nx1,1,1);
 
 
-%Dynamic spins
-%GLYCERINE
+%Tissue 1
 M0(1:Nx0,:,:)=1;
-T1(1:Nx0,:,:)=144; %T1 eau+glycerine = 1154 ms, T1 Eau+Glycerine+Gado = 144 ms
-T2(1:Nx0,:,:)=2;   %T2 eau+glycerine = 192 ms, T1 Eau+Glycerine+Gado = 86 ms
-T2s(1:Nx0,:,:)=2;
+T1(1:Nx0,:,:)=144; %T1 water+glycerol = 1154 ms, T1 water+Glycerol+Gadolinium = 144 ms
+T2(1:Nx0,:,:)=10;   %T2 water+glycerol = 192 ms, T1 water+Glycerol+Gadolinium = 86 ms
+T2s(1:Nx0,:,:)=10;
 DB(1:Nx0,:,:)=0;
 OFFSET=[0 0 0];
 RES=1;
 
-%Static spins
-%EAU PURE
+%Tissue 2
 M0(Nx0+1:Nx1,:,:)=1;
-T1(Nx0+1:Nx1,:,:)=2885; %T1 eau liquide  2885 ms
-T2(Nx0+1:Nx1,:,:)=2;
-T2s(Nx0+1:Nx1,:,:)=2;
+T1(Nx0+1:Nx1,:,:)=2885; %T1 water  2885 ms
+T2(Nx0+1:Nx1,:,:)=10;
+T2s(Nx0+1:Nx1,:,:)=10;
 DB(Nx0+1:Nx1,:,:)=0;
 OFFSET=[0 0 0];
 RES=1;
 
-%Dynamic spins
-%SANG
+%Blood
 M0(Nx1+1:Nx2,:,:)=1;
-T1(Nx1+1:Nx2,:,:)=1584; %T1 Veineux 1584 ms  Arteriel 1664 ms
-T2(Nx1+1:Nx2,:,:)=2;  %T2 275 ms
-T2s(Nx1+1:Nx2,:,:)=2;
+T1(Nx1+1:Nx2,:,:)=1584; %T1 Venous 1584 ms  Arterial 1664 ms
+T2(Nx1+1:Nx2,:,:)=10;  %T2 275 ms
+T2s(Nx1+1:Nx2,:,:)=10;
 DB(Nx1+1:Nx2,:,:)=0;
 OFFSET=[0 0 0];
 RES=1;
